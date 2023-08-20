d8aecddefd79f9dc7edbab6eb58c12715d3825d4
LeandroFGA1 <aucache.leandro@gmail.com>
2023-08-20 20:51:49 -0300

    dolarbar v1

---
+++ index.js
@@ -0,0 +1,28 @@
+const wea = (e) => {
+    e.preventDefault();
+    let globalData; // Variable global
+    fetch("https://dolarapi.com/v1/dolares")
+        .then(response => response.json())
+        .then(data => {
+            globalData = data; // ALmacenar los datos
+            console.log(globalData); //sacar luego
+            const weaContainer = document.getElementById("dolar-api");
+            weaContainer.innerHTML = globalData.slice(0,4).map(parte =>
+                `<div class="dolar-item ${parte.nombre}-container">
+                    <h2>${parte.nombre === "Contado con liquidación" ? "Cdo C/liq" : parte.nombre}</h2>
+                    <div class="dolar-value-container">
+                        <span>Compra: ${parte.compra ||"--"}</span>
+                        <span>Venta: ${parte.venta || "--"}</span>
+                    </div>
+                </div>`
+            ).join("");
+        });
+}
+
+
+
+
+const init = () =>{
+    document.addEventListener("DOMContentLoaded",wea)
+}
+init();
